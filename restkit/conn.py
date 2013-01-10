eebd52e977a73872a4e247ab08cff8666f7cd4c0
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2013-01-10 11:36:52 +0100

    use the new socketpool ssl connector

--- restkit/conn.py
+++ restkit/conn.py
@@ -3,64 +3,24 @@
 # This file is part of restkit released under the MIT license.
 # See the NOTICE for more information.
 
-import logging
-import random
-import select
-import socket
-import ssl
-import time
-
-from socketpool import Connector
-from socketpool.util import is_connected
+from socketpool.conn import TcpSSLConnector
 
 CHUNK_SIZE = 16 * 1024
 MAX_BODY = 1024 * 112
 DNS_TIMEOUT = 60
 
 
-class Connection(Connector):
+class Connection(TcpSSLConnector):
 
     def __init__(self, host, port, backend_mod=None, pool=None,
-            is_ssl=False, extra_headers=[],  **ssl_args):
+                 is_ssl=False, extra_headers=[],  **ssl_args):
 
-        # connect the socket, if we are using an SSL connection, we wrap
-        # the socket.
-        self._s = backend_mod.Socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._s.connect((host, port))
-        if is_ssl:
-            self._s = ssl.wrap_socket(self._s, **ssl_args)
-
-
+        super(Connection, self).__init__(
+            host, port, backend_mod, pool, is_ssl, **ssl_args)
         self.extra_headers = extra_headers
-        self.is_ssl = is_ssl
-        self.backend_mod = backend_mod
-        self.host = host
-        self.port = port
-        self._connected = True
-        self._life =  time.time() - random.randint(0, 10)
-        self._pool = pool
-        self._released = False
-
-    def matches(self, **match_options):
-        target_host = match_options.get('host')
-        target_port = match_options.get('port')
-        return target_host == self.host and target_port == self.port
-
-    def is_connected(self):
-        if self._connected:
-            return is_connected(self._s)
-        return False
 
     def handle_exception(self, exception):
         raise
-
-    def get_lifetime(self):
-        return self._life
-
-    def invalidate(self):
-        self.close()
-        self._connected = False
-        self._life = -1
 
     def release(self, should_close=False):
         if self._pool is not None:
@@ -74,12 +34,8 @@ class Connection(Connector):
             self.invalidate()
 
     def close(self):
-        if not self._s or not hasattr(self._s, "close"):
-            return
-        try:
-            self._s.close()
-        except:
-            pass
+        #XXX: warn for missuse
+        self.invalidate()
 
     def socket(self):
         return self._s
@@ -98,7 +54,6 @@ class Connection(Connector):
         for line in list(lines):
             self.send(line, chunked=chunked)
 
-
     # TODO: add support for sendfile api
     def sendfile(self, data, chunked=False):
         """ send a data from a FileObject """
@@ -112,6 +67,3 @@ class Connection(Connector):
                 break
             self.send(binarydata, chunked=chunked)
 
-
-    def recv(self, size=1024):
-        return self._s.recv(size)
